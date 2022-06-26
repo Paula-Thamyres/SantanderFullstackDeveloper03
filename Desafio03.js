3a5342025a886097887b4eeafb20eb286237950f
Paula Thamyres da Silva <84049116+Paula-Thamyres@users.noreply.github.com>
2022-06-26 15:50:25 -0300

    Desafio 03 - SantanderBootcampFullstackDeveloper

---
+++ Desafio03.js
@@ -0,0 +1,10 @@
+let lines = gets().split("\n");
+
+var line = lines.shift().split(' ');
+var T1 = parseInt(line[0]);
+var T2 = parseInt(line[1]);
+var T3 = parseInt(line[2]);
+var T4 = parseInt(line[3]);
+
+var totalTomadas = (T1 + T2 + T3 + T4) - 3;
+console.log(totalTomadas);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SantanderFullstackDeveloper03
 Desafio 03 - Santander Bootcamp Fullstack Developer
+
+Resolução do dessafio utilizando javascript com array, para retorno em cada número inserido.
